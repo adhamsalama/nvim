eca3565d56e23a2d02464aa2f7a20ae68b2b7079
Adham Salama <adhamsalama@zohomail.com>
2025-01-20 08:31:26 +0200

    Add nvim

---
+++ nvim/lua/polish.lua
@@ -0,0 +1,20 @@
+--if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- This will run last in the setup process and is a good place to configure
+-- things like custom filetypes. This is just pure lua so anything that doesn't
+-- fit in the normal config locations above can go here
+
+-- Set up custom filetypes
+--[[vim.filetype.add {
+  extension = {
+    foo = "fooscript",
+  },
+  filename = {
+    ["Foofile"] = "fooscript",
+  },
+  pattern = {
+    ["~/%.config/foo/.*"] = "fooscript",
+  },
+}--]]
+
+vim.opt.guicursor = "n-v-c-sm:block,ci-ve:ver25,r-cr-o:hor20,i:block-blinkwait700-blinkoff400-blinkon250-Cursor/lCursor"
