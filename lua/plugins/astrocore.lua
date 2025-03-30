bce97ca789d02384618d50b991f9230e8e3ba4b9
Adham Salama <adhamsalama@zohomail.com>
2025-03-30 16:24:41 +0200

    Disable gc and gC mappings

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -72,7 +72,8 @@ return {
           desc = "Close tab",
         },
         ["<Leader>gT"] = false,
-        ["<leader>gC"] = { "<cmd>Git commit<cr>", desc = "Git Commit" },
+        ["<leader>gc"] = false,
+        ["<leader>gC"] = false,
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
         -- ["<Leader>b"] = { desc = "Buffers" },
