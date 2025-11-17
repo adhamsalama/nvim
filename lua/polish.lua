b3f2a37e20507442302f1982d77d0221a41522e2
Adham Salama <adhamsalama@zohomail.com>
2025-11-18 00:08:56 +0200

    feat: remove col number from quickfix list entires

--- lua/polish.lua
+++ lua/polish.lua
@@ -20,3 +20,44 @@
 -- vim.keymap.set("n", "<leader>gd", "<cmd>DiffviewOpen<cr>", { desc = "View Git diff" })
 
 vim.opt.guicursor = "n-v-c-sm:block,ci-ve:ver25,r-cr-o:hor20,i:block-blinkwait700-blinkoff400-blinkon250-Cursor/lCursor"
+
+-- Custom quickfix text function to hide column numbers
+_G.qftextfunc_no_col = function(info)
+  local items
+  local ret = {}
+  if info.quickfix == 1 then
+    items = vim.fn.getqflist({ id = info.id, items = 0 }).items
+  else
+    items = vim.fn.getloclist(info.winid, { id = info.id, items = 0 }).items
+  end
+
+  for i = info.start_idx, info.end_idx do
+    local e = items[i]
+    local str
+    if e.valid == 1 then
+      local fname = ""
+      if e.bufnr > 0 then
+        fname = vim.fn.bufname(e.bufnr)
+        if fname == "" then
+          fname = "[No Name]"
+        else
+          fname = fname:gsub("^" .. vim.env.HOME, "~")
+        end
+      end
+
+      local lnum = e.lnum > 0 and e.lnum or 0
+      local text = e.text or ""
+
+      -- Format: filename|line| text (without column)
+      if fname ~= "" then
+        str = string.format("%s|%d| %s", fname, lnum, text)
+      else
+        str = string.format("|%d| %s", lnum, text)
+      end
+    else
+      str = e.text
+    end
+    table.insert(ret, str)
+  end
+  return ret
+end
